49e3ef01130c51b5cdd81317903168e670b0f194
conantsai <conantsai850605@gmail.com>
2018-12-10 00:12:59 -0500

    Add files via upload

---
+++ IP_ProgramExample_C#/DIP_ImageProcessExample/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DIP_ImageProcessExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        Bitmap openImg;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //openFileDialog1.InitialDirectory = "C:";
+            openFileDialog1.Filter = "All Files|*.*|Bitmap Files (.bmp)|*.bmp|Jpeg File(.jpg)|*.jpg";
+            // 選擇我們需要開檔的類型
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            { // 如果成功開檔
+                openImg = new Bitmap(openFileDialog1.FileName);
+                // 宣告存取影像的 bitmap
+                pictureBox1.Image = openImg;
+                // 讀取的影像展示到 pictureBox
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "All Files|*.*|Bitmap Files (.bmp)|*.bmp|Jpeg File(.jpg)|*.jpg";
+
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                openImg.Save(sfd.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int y = 0; y < openImg.Height; y++)
+            {
+                for (int x = 0; x < openImg.Width; x++)
+                {
+                    // 讀取影像平面上(x,y)的RGB資訊
+                    Color RGB = openImg.GetPixel(x, y);
+                    // RGB 是 VS 內建的 class 可以直接讀取影像的色彩資訊 R = Red, G = Green, B =Blue                        
+                    int invR = Convert.ToInt32(255 - RGB.R);
+                    int invG = Convert.ToInt32(255 - RGB.G);
+                    int invB = Convert.ToInt32(255 - RGB.B);
+
+                    openImg.SetPixel(x, y, Color.FromArgb(invR, invG, invB));
+                }
+            }
+            pictureBox1.Image = openImg;
+        }
+    }
+}
